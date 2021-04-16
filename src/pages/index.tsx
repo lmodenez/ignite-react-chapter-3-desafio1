e61c34a80f925dad234d8a7088f2bdebee0edbd7
Luiz Pedro Modenez <lmodenez@starplast.matriz>
2021-04-16 16:28:04 -0300

    feat: add config prismic and return posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Head from 'next/head';
 
 interface Post {
   uid?: string;
@@ -24,13 +27,38 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Challenge</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+    </>
+  )
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type','posts')],
+    {
+      fetch: ['posts.title'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = postsResponse.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: post.data.title
+    }
+  })
+
+  console.log(posts);
+
+  return {
+    props: { posts },
+  };
+};
