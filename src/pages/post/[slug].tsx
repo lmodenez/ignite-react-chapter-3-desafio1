25ef200f5ab2456dfb82c7367f9a8512828cd6ed
Luiz Pedro Modenez <lmodenez98@gmail.com>
2021-04-19 21:35:51 -0300

    fix: post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -26,9 +26,9 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  // TODO
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
